39feb58bfc8aa2329bd232a3108c03edf1d4ac78
YingyinZhang <867497302@qq.com>
2020-11-15 21:04:21 +0800

    add lab8 new files

--- app/Controllers/Travel.php
+++ app/Controllers/Travel.php
@@ -4,26 +4,45 @@ namespace App\Controllers;
 class Travel extends BaseController {
 
     public function index() {
+        $places = new \App\Models\Places(); 
+        $table = new \CodeIgniter\View\Table();
+        $headings = $places->fields;
+        $displayHeadings = array_slice($headings, 1, 2);
+        $table->setHeading(array_map('ucfirst', $displayHeadings));
+            $records = $places->findAll(); 
+        foreach ($records as $record) {
+        $nameLink = anchor("travel/showme/$record->id",$record->name);
+        $table->addRow($nameLink,$record->description);
+       // $table->addRow($record->name,$record->description);
+        }
+        $template = [
+        'table_open' => '<table cellpadding="5px">',
+        'cell_start' => '<td style="border: 1px solid #dddddd;">',
+        'row_alt_start' => '<tr style="background-color:#dddddd">',
+        ];
+        $table->setTemplate($template);
+        $fields = [
+        'title' => 'Travel Destinations',
+        'heading' => 'Travel Destinations',
+        'footer' => 'Copyright Xavier'
+        ];
         // connect to the model     
-        $places = new \App\Models\Places();   
         // retrieve all the records    
-        $records = $places->findAll(); 
-        
         // get a template parser    
         $parser = \Config\Services::parser();  
         // tell it about the substitions   
-        return $parser->setData(['records' => $records])      
-        // and have it render the template with those 
-        ->render('placeslist'); 
+      return $parser->setData($fields)
+        ->render('templates\top') .
+        $table->generate() .
+        $parser->setData($fields)
+        ->render('templates\bottom');
     }
-     public function showme($id)   
+    public function showme($id)   
      {   
         // connect to the model  
         $places = new \App\Models\Places();  
         // retrieve all the records   
-        $record = $places->find($id);
-         
-        
+        $record = $places->find($id);   
         // get a template parser    
         $parser = \Config\Services::parser();   
         // tell it about the substitions   
