a934cd02987b0ac521fc3042d814fc7a4e624ed5
YingyinZhang <867497302@qq.com>
2020-11-22 21:33:59 +0800

    the index page change the home page to the travel page(Rock Singer)

--- app/Controllers/Travel.php
+++ app/Controllers/Travel.php
@@ -9,13 +9,13 @@ class Travel extends BaseController {
         $places = new \App\Models\Places();
         $table = new \CodeIgniter\View\Table();
         $headings = $places->fields;
-        $displayHeadings = array($headings[1], $headings[7], $headings[6]);
+        $displayHeadings = array($headings[1], $headings[7]);
         $table->setHeading(array_map('ucfirst', $displayHeadings));
         $records = $places->findAll();
         foreach ($records as $record) {
             $nameLink = anchor("travel/showme/$record->id", $record->name);
             $image = '<img src="/image/' . $record->image . '">';
-            $table->addRow($nameLink, $image, $record->description);
+            $table->addRow($nameLink, $image);
             // $table->addRow($record->name,$record->description);
         }
         $template = [
@@ -25,9 +25,9 @@ class Travel extends BaseController {
         ];
         $table->setTemplate($template);
         $fields = [
-            'title' => 'Travel Destinations',
-            'heading' => 'Travel Destinations',
-            'footer' => 'Copyright Xavier'
+            'title' => 'Rock Singer',
+            'heading' => 'Rock Singer',
+            'footer' => 'Copyright ZYY'
         ];
         // connect to the model     
         // retrieve all the records    
@@ -43,11 +43,42 @@ class Travel extends BaseController {
 
     public function showme($id) {
         // connect to the model  
-        $places = new \App\Models\Places();
+        $places =new \App\Models\Places();
         // retrieve all the records   
         $record = $places->find($id);
         
         $table = new \CodeIgniter\View\Table();
+        
+        
+           $table->addRow($record['id']);
+           $table->addRow($record['name']);
+           $table->addRow($record['birth']);
+           $table->addRow($record['place']);
+           $table->addRow($record['description']);
+           $table->addRow($record['magnum opus']);
+           $table->addRow($record['link']);
+            $image = '<img src="/image/' . $record['image'] .'">';
+            $table->addRow($image);
+            
+        $template = [
+            'table_open' => '<table cellpadding="5px">',
+            'cell_start' => '<td style="border: 1px solid #dddddd;">',
+            'row_alt_start' => '<tr style="background-color:#dddddd">',
+        ];
+        $table->setTemplate($template);
+        $fields = [
+            'title' => 'Rock Singer',
+            'heading' => 'Rock Singer',
+            'footer' => 'Copyright ZYY'
+        ];
+        $parser = \Config\Services::parser();
+        // tell it about the substitions   
+        return $parser->setData($fields)
+                        ->render('templates\top') .
+                $table->generate() .
+                        $parser->setData($fields)
+                        ->render('templates\bottom');
+    
         // get a template parser    
         $parser = \Config\Services::parser();
         // tell it about the substitions   
@@ -55,5 +86,6 @@ class Travel extends BaseController {
                         // and have it render the template with those      
                         ->render('oneplace');
     }
+   
 
 }
