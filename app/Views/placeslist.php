245e0d8c875f54b5a8acb40e2edaf9ab70affc87
YingyinZhang <867497302@qq.com>
2020-11-22 21:26:46 +0800

    change the title ;Rock singer

--- app/Views/placeslist.php
+++ app/Views/placeslist.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html>
 	<head>
-		<title>DGPT4711 Assignment</title>
+		<title>DGPT4711 Assignment1</title>
 	</head>
 	<body>
-		<h1>List of Travel Destinations</h1>
+		<h1>Rock singer</h1>
 		{records}
 		<p><a href="/travel/showme/{id}">{name}</a></p>
 		{/records}
